226ba168cf5a8879ca770886d23feac0fa519d3e
AnastasiiaMurzina <hloshkin@mail.ru>
2017-09-16 21:36:07 +0300

    one array with double index

--- openedu_asd_l1_4.py
+++ openedu_asd_l1_4.py
@@ -1,16 +1,14 @@
 with open('input.txt','r') as f:
     length = int(f.readline())
     array = list(map(lambda x: float(x),f.read().split()))
-positions=[i for i in range(1,length+1)]
+array = [ [array[j-1],j] for j in range(1, length+1)]
+# print(array)
 for i in range(length-1):
     j = i+1
-    while j>0 and array[j]<array[j-1]:
+    while j>0 and array[j][0]<array[j-1][0]:
         tmp = array[j]
         array[j]=array[j-1]
         array[j-1]=tmp
-        tmp_extra = positions[j]
-        positions[j]=positions[j-1]
-        positions[j-1]=tmp_extra
         j-=1
 with open('output.txt','w') as f1:
-    f1.write(str(positions[0])+' '+str(positions[length//2])+' '+str(positions[-1]))
+    f1.write(str(array[0][1])+' '+str(array[length//2][1])+' '+str(array[-1][1]))
